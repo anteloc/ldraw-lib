c7cc1b282b52da8c4393d541cd13df6559b8c280
anteloc <6914284+anteloc@users.noreply.github.com>
2026-08-04 00:34:29 +0200

    Minifig props to glb, glb indexes names now with .glb extension

--- scripts/prepare-glb-files.sh
+++ scripts/prepare-glb-files.sh
@@ -16,16 +16,18 @@ function usage() {
 }
 
 function add_glb_size() {
+    local glb_dir="$1"
     # make this input pipeable, so it can be used as a filter to add the glb_size to each line of a jsonl file
     cat | while IFS= read -r json_line; do
-        add_glb_size_to_json "$json_line"
+        add_glb_size_to_json "$glb_dir" "$json_line"
     done
 }
 
 function add_glb_size_to_json() {
-    local json_line="$1"
+    local glb_dir="$1"
+    local json_line="$2"
     local model_name=$(echo "$json_line" | jq -r '.name')
-    local glb_file="$parent_dir/models-glb/${model_name%.mpd.zip}.glb"
+    local glb_file="$glb_dir/${model_name%.mpd.zip}.glb"
     if [ -f "$glb_file" ]; then
         local glb_size=$(stat -f%z "$glb_file")
         local glb_size_kb=$((glb_size / 1024))
@@ -45,6 +47,48 @@ fi
 
 cd "$parent_dir"
 
+### Models
+
+# create a temp file to store the names of the .glb model files, but replacing extension with .mpd.zip
+tmp_file=$(mktemp)
+
+ls -1 models-glb | sed 's/\.glb/.mpd.zip/g' > "$tmp_file"
+
+# Create models-glb-index.jsonl: use the models index plus add size of the .glb files
+grep -F -f "$tmp_file" 'models-index.jsonl' | add_glb_size 'models-glb' | sed 's/\.mpd\.zip/.glb/g' > 'models-glb-index.jsonl'
+
+rm "$tmp_file"
+
+
+### Minifigs
+
+# Create convert minifig-props to glb and create minifig-props-glb-index.jsonl: use the minifig-props index plus add size of the .glb files
+# This we will create from scratch, not based on a non-glb index
+
+minifig_idx_entry_tmpl='{"category":"Minifig","description":"%s","keywords":["minifig","prop","tool","weapon","shield"],"name":"%s","glb_size_kb":%d}'
+
+[ -f 'minifig-props-glb-index.jsonl' ] && echo "Deleting minifig-props-glb-index.jsonl..." && rm 'minifig-props-glb-index.jsonl'
+
+for d in ./minifig-props/*.dat; do 
+    prop_name=$(basename "$d" .dat)
+    prop_desc=$(head -n 1 "$d" | tr -d '\r' | sed 's/^0 Minifig //') # first line of the .dat file, without the leading "0 Minifig "
+    
+    glb_file="./minifig-props-glb/${prop_name}.glb"
+
+    mpd2glb.sh -l ldraw -c meshopt -o "$glb_file" "$d" 2>/dev/null
+
+    if [ -f "$glb_file" ]; then
+        glb_size=$(stat -f%z "$glb_file")
+        glb_size_kb=$((glb_size / 1024))
+        printf "$minifig_idx_entry_tmpl\n" "$prop_desc" "$prop_name.glb" "$glb_size_kb" >> 'minifig-props-glb-index.jsonl'
+    fi
+done 
+
+## Thumbnails
+
+# minifig-props thumbnails, 400x400, just in case new props were added
+scripts/create-thumbnails.sh ./minifig-props ./thumbnails 400x400
+
 # Use GitHub LFS, large files can be downloaded from raw urls too, but it could deplete LFS download budget! 
 [ -f 'thumbnails.zip' ] && echo "Deleting thumbnails.zip archive..." && rm 'thumbnails.zip'
 [ -f 'thumbnails-small.zip' ] && echo "Deleting thumbnails-small.zip archive..." && rm 'thumbnails-small.zip'
@@ -54,15 +98,5 @@ echo "Zipping thumbnails directories into thumbnails.zip and thumbnails-small.zi
 zip -r 'thumbnails.zip' 'thumbnails'
 zip -r 'thumbnails-small.zip' 'thumbnails-small'
 
-# create a temp file to store the names of the .glb model files, but replacing extension with .mpd.zip
-tmp_file=$(mktemp)
 
-ls -1 models-glb | sed 's/\.glb/.mpd.zip/g' > "$tmp_file"
-
-# grep the models-index.jsonl file, filtering by exact matches on the tmp_file
-grep -F -f "$tmp_file" 'models-index.jsonl' | add_glb_size > 'models-glb-index.jsonl'
-
-
-rm "$tmp_file"
-
-echo "GLB required files prepared. Models index file created at: models-glb-index.jsonl"
+echo "GLB required files prepared. Indexes created at: models-glb-index.jsonl and minifig-props-glb-index.jsonl"
