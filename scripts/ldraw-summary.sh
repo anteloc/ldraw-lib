93630015fac49c3ed5132daede035c2bc471af5e
anteloc <6914284+anteloc@users.noreply.github.com>
2026-09-04 23:17:40 +0200

    All .glb models now with draco compression, instead of meshopt

--- scripts/ldraw-summary.sh
+++ scripts/ldraw-summary.sh
@@ -10,18 +10,17 @@ COLOURS_FILE="$parent_dir/ldraw/LDConfig.ldr"
 
 function usage() {
     echo "Usage: $(basename "$0") <--colour <colour_name or colour_code> | --model <model_name> | --part <part_name> | --submodel <model_name> <submodel_name>>"
-    echo "Returns the source code for either a part, a model or a submodel from a model, or if given a colour name it returns the code, and viceversa."
-    echo "Example: "
-    echo "$(basename "$0") --part 71944.dat"
-    echo "$(basename "$0") --model 3001.mpd"
-    echo "$(basename "$0") --submodel 3001.mpd '41606 - Step 30.ldr'"
-    echo "$(basename "$0") --colour Blue # returns: 1"
-    echo "$(basename "$0") --colour 1 # returns: Blue"
-    echo "$(basename "$0") --submodel 3001.mpd '41606 - Step 30.ldr'"
+    echo "Returns a summary for either a part, a model or a submodel from a model, or if given a colour name it returns the code, and viceversa."
+    echo "Execute the following examples for real outputs:"
+    echo "$(basename "$0") --part 3832.dat"
+    echo "$(basename "$0") --model 106-1.mpd"
+    echo "$(basename "$0") --submodel 106-1.mpd '106 - Minifig.ldr'"
+    echo "$(basename "$0") --colour Blue"
+    echo "$(basename "$0") --colour 10"
     exit 1
 }
 
-function colour_src() {
+function colour_summary() {
     local name_or_code="$1"
     found_name_or_code=$(awk -v name_or_code="$name_or_code" '
         $1 == "0" && $2 == "!COLOUR" && toupper($3) == toupper(name_or_code) { print $5; exit;}
@@ -31,20 +30,41 @@ function colour_src() {
         echo "Colour not found: '$name_or_code'" >&2
         exit 1
     fi
-    echo "$found_name_or_code"
+    echo "$name_or_code -> $found_name_or_code"
 }
 
-function model_src() {
+function parts_bom() {
+    cat | grep '0 //' | sort | uniq -c | sed 's|0 //|x|' | xargs -I{} echo "  * {}"
+}
+
+function main_header() {
+    local item_type="$1"
+    local item_name="$2"
+    echo "# ${item_type}: $item_name"
+    echo ""
+}
+
+function submodel_header() {
+    cat | grep -A 1 ' FILE ' | sed -e 's/0 FILE/## Submodel:/' -e 's/^0 /\n**Description:** \n\n  /'
+}
+
+function model_summary() {
     local model_name="$1"
     local model_file="$MODELS_DIR/$model_name"
     if [[ ! -f "$model_file" ]]; then
         echo "Model file not found: '$model_file'" >&2
         exit 1
     fi
-    cat "$model_file"
+
+    subs="$(cat "$model_file" | grep '0 FILE' | sed 's/0 FILE //' | awk '{$1=$1;print}')"
+
+    echo "$subs" | while IFS= read -r sm; do 
+        submodel_summary "$model_name" "$sm"; 
+        echo ""
+    done
 }
 
-function submodel_src() {
+function submodel_summary() {
     local model_name="$1"
     local submodel_name="$2"
     local model_file="$MODELS_DIR/$model_name"
@@ -72,17 +92,23 @@ function submodel_src() {
         echo "Submodel not found: '$submodel_name'" >&2
         exit 1
     fi
-    echo "$submodel_src"
+
+
+    echo "$submodel_src" | submodel_header
+    echo ""
+    echo "**Parts BOM:**"
+    echo ""
+    echo "$submodel_src" | parts_bom
 }
 
-function part_src() {
+function part_summary() {
     local part_name="$1"
     local part_file="$PARTS_DIR/$part_name"
     if [[ ! -f "$part_file" ]]; then
         echo "Part file not found: '$part_file'" >&2
         exit 1
     fi
-    cat "$part_file"
+    cat "$part_file" | head -1 | sed 's/^0 /**Description:** /'
 }
 
 query_type=""
@@ -122,16 +148,19 @@ fi
 
 case "$query_type" in
     model)
-        model_src "$model_name"
+        main_header "Model" "$model_name"
+        model_summary "$model_name"
         ;;
     part)
-        part_src "$part_name"
+        main_header "Part" "$part_name"
+        part_summary "$part_name"
         ;;
     submodel)
-        submodel_src "$model_name" "$submodel_name"
+        main_header "Model" "$model_name"
+        submodel_summary "$model_name" "$submodel_name"
         ;;
     colour)
-        colour_src "$colour_name_or_code"
+        colour_summary "$colour_name_or_code"
         ;;
     *)
         usage
